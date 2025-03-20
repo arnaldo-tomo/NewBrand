3559ba0a5cd6990f4e1bd6c9b8c1839221b11347
Arnaldo tomo <arnaldotomo@gmail.com>
2025-03-20 16:45:28 +0200

    Create menu.php

---
+++ resources/lang/pt/menu.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'home' => 'Início',
+    'about' => 'Sobre',
+    'projects' => 'Projetos',
+    'education' => 'Educação',
+    'testimonials' => 'Depoimentos',
+    'partners' => 'Parceiros',
+    'blog' => 'Blog',
+    'contact' => 'Contacto',
+];

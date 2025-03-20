7003941dc96cedef21fceb9a0558f67b42690d53
Arnaldo tomo <arnaldotomo@gmail.com>
2025-03-20 16:45:13 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,12 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
         $middleware->append(SetLocale::class);
-        //...
-
+        
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
 
     })->create();

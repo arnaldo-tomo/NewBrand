bf1743510c62b6b4d72521b533a55432d4794259
Arnaldo tomo <arnaldotomo@gmail.com>
2025-03-20 16:45:26 +0200

    Create home.php

---
+++ resources/lang/en/home.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'software_engineer' => 'SOFTWARE ENGINEER.',
+    'specialized_in' => 'Specialized in Laravel and React Native, I develop innovative technological solutions for companies, NGOs.',
+];

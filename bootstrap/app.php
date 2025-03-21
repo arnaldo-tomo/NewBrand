a13312403490d20ef43704d87fa41dc87368a759
Arnaldo tomo <arnaldotomo@gmail.com>
2025-03-21 09:01:29 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\BlockDetectionBots;
+use App\Http\Middleware\MinifyHtml;
 use App\Http\Middleware\SetLocale;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -13,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(SetLocale::class);
+        $middleware->append(BlockDetectionBots::class);
+        $middleware->append(MinifyHtml::class);
         
     })
     ->withExceptions(function (Exceptions $exceptions) {

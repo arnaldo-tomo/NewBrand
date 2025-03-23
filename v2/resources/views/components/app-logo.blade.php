61999b267126fa16891eadf5f7290e98217b9df6
Arnaldo tomo <arnaldotomo@gmail.com>
2025-03-23 23:06:05 +0200

    Create app-logo.blade.php

---
+++ v2/resources/views/components/app-logo.blade.php
@@ -0,0 +1,6 @@
+<div class="flex aspect-square size-8 items-center justify-center rounded-md ">
+    <x-app-logo-icon class="size-5 fill-current dark:text-black" />
+</div>
+<div class="ml-1 grid flex-1 text-left text-sm">
+    <span class="mb-0.5 truncate leading-none font-semibold">Arnaldo Tomo</span>
+</div>

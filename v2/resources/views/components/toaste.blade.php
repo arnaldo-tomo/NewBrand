c6a3cca711446c5c69b15dd59841f06ebc0fa66f
Arnaldo tomo <arnaldotomo@gmail.com>
2025-03-23 23:06:14 +0200

    Create toaste.blade.php

---
+++ v2/resources/views/components/toaste.blade.php
@@ -0,0 +1,12 @@
+<div id="dev-toast" class="dev-toast">
+    <div class="toast-icon">
+        <i class="fas fa-shield-virus"></i>
+    </div>
+    <div class="toast-content">
+        <div class="toast-title">Aviso de Segurança</div>
+        <div class="toast-message">Este website coleta informações do seu dispositivo para provar que você (e ninguém) está realmente seguro na internet.<br/> Mas relaxe, só quero mostrar minhas habilidades!</div>
+    </div>
+    <button class="toast-close" id="close-toast">
+        <i class="fas fa-times"></i>
+    </button>
+</div>
